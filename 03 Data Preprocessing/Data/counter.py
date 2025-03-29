39f6debebe5f92cd540f87a8f918eb5ba3228d30
Harshit Soni <harshit11664@gmail.com>
2025-03-29 15:41:42 +0530

    added garvit's data

--- 03 Data Preprocessing/Data/counter.py
+++ 03 Data Preprocessing/Data/counter.py
@@ -23,26 +23,52 @@ def count_rows_in_csv_files(folder_path):
     
     return row_counts
 
-def get_count(folder_path):
+def get_count(folder_path, print_each_csv_count=False, save_to_file=False):
+    """
+    This function counts the number of rows in all CSV files in a given folder."
+    """
+    # Check if the folder exists
+    if not os.path.exists(folder_path):
+        print(f"Folder '{folder_path}' does not exist.")
+        return
+    # Check if the folder is empty
+    if not os.listdir(folder_path):
+        print(f"Folder '{folder_path}' is empty.")
+        return
     # Get the row counts
     row_counts = count_rows_in_csv_files(folder_path)
     total_data = sum(row_counts.values())
     # Print the row counts
     for csv_file, row_count in row_counts.items():
-        print(f'{csv_file}: {row_count} rows')
-    print(f'Total data: {total_data} rows\n')
+        if print_each_csv_count:
+            print(f'{csv_file}: {row_count} rows')
+    print(f'Total data of {folder_path}: {total_data} rows\n')
+    # Save the row counts to a file
+    if save_to_file:
+        output_file = os.path.join(folder_path, 'row_counts.txt')
+        with open(output_file, 'w') as file:
+            for csv_file, row_count in row_counts.items():
+                file.write(f'{csv_file}: {row_count} rows\n')
+            file.write(f'Total data of {folder_path}: {total_data} rows\n')
+        print(f'Row counts saved to {output_file}')
+
 
 
 
 # Specify the folder path
-Bitumin = 'C:/Users/harsh/OneDrive/Documents/nn data/Bitumin'
-Block = 'C:/Users/harsh/OneDrive/Documents/nn data/Block'
-Concrete = 'C:/Users/harsh/OneDrive/Documents/nn data/Concrete'
-Kanker = 'C:/Users/harsh/OneDrive/Documents/nn data/Kanker'
+Bitumin = 'Bitumin'
+Block = 'Block'
+Concrete = 'Concrete'
+Kanker = 'Kanker'
+MultiSB = 'MultiSB'
+SingleSB = 'SingleSB'
 
 
 # call get_count function for each folder
+print('\nCounting the dataset...\n\n')
 get_count(Bitumin)
 get_count(Block)
 get_count(Concrete)
 get_count(Kanker)
+get_count(MultiSB)
+get_count(SingleSB)
